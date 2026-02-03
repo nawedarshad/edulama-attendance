3f04ba34720586c42bba8627fd08a3a6e53ec67c
craftr <nawedarshad25@gmail.com>
2026-02-03 18:53:47 +0530

    Initialize

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('attendance');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   // app.useGlobalFilters(new GlobalExceptionFilter());
   app.enableCors({
